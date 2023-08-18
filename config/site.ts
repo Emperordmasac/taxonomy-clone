fa222e588b55d75932b1aaeee9fc0a8846058ec7
Abiola Olamilekan <abiolawalter2015@gmail.com>
2023-08-19 00:21:34 +0100

    InProgress: contentlayer

---
+++ config/site.ts
@@ -0,0 +1,9 @@
+import { SiteConfig } from 'types/config'
+
+export const siteConfig: SiteConfig = {
+  name: 'Pavilion',
+  links: {
+    twitter: 'https://twitter.com/horlaymilekan',
+    github: 'https://github.com/emperordmasac',
+  },
+}
