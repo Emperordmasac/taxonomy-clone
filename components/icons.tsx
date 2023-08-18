d7fbba40befba3aaf6991bcd61d493c8ee9a3387
Abiola Olamilekan <abiolawalter2015@gmail.com>
2023-08-18 16:02:54 +0100

    feat: implement responsive styles for marketing pages

--- components/icons.tsx
+++ components/icons.tsx
@@ -14,6 +14,7 @@ import {
   AlertTriangle,
   HelpCircle,
   Pizza,
+  Github,
 } from "lucide-react";
 
 import type { Icon as lucideIcon } from "lucide-react";
@@ -36,4 +37,5 @@ export const Icons = {
   warning: AlertTriangle,
   help: HelpCircle,
   pizza: Pizza,
+  github: Github,
 };
