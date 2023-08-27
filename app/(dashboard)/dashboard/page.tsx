f7b3bd5a302952188530d47191ceff2e602dc8d1
Abiola Olamilekan <abiolawalter2015@gmail.com>
2023-08-27 21:13:44 +0100

    feat: implement caching for user dashboard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,16 +1,18 @@
-import { redirect } from "next/navigation";
-import { getCurrentUser } from "@/lib/session";
-import { User } from "@prisma/client";
-import { authOptions } from "@/lib/auth";
-import { db } from "@/lib/db";
+import { redirect } from "next/navigation"
+import { cache } from "react"
 
-import { DashboardHeader } from "@/components/dashboard/header";
-import { PostCreateButton } from "@/components/dashboard/post-create-button";
-import { DashboardShell } from "@/components/dashboard/shell";
-import { PostItem } from "@/components/dashboard/post-item";
-import { EmptyPlaceholder } from "@/components/dashboard/empty-placeholder";
+import { db } from "@/lib/db"
+import { getCurrentUser } from "@/lib/session"
+import { User } from "@prisma/client"
+import { authOptions } from "@/lib/auth"
 
-async function getPostsForUser(userId: User["id"]) {
+import { DashboardHeader } from "@/components/dashboard/header"
+import { PostCreateButton } from "@/components/dashboard/post-create-button"
+import { DashboardShell } from "@/components/dashboard/shell"
+import { PostItem } from "@/components/dashboard/post-item"
+import { EmptyPlaceholder } from "@/components/dashboard/empty-placeholder"
+
+const getPostsForUser = cache(async (userId: User["id"]) => {
   return await db.post.findMany({
     where: {
       authorId: userId,
@@ -24,17 +26,17 @@ async function getPostsForUser(userId: User["id"]) {
     orderBy: {
       updatedAt: "desc",
     },
-  });
-}
+  })
+})
 
 export default async function DashboardPage() {
-  const user = await getCurrentUser();
+  const user = await getCurrentUser()
 
   if (!user) {
-    redirect(authOptions.pages.signIn);
+    redirect(authOptions.pages.signIn)
   }
 
-  const posts = await getPostsForUser(user.id);
+  const posts = await getPostsForUser(user.id)
 
   return (
     <DashboardShell>
@@ -43,7 +45,7 @@ export default async function DashboardPage() {
       </DashboardHeader>
       <div>
         {posts?.length ? (
-          <div className="divide-y divide-neutral-200 rounded-md border border-slate-200">
+          <div className="border divide-y rounded-md divide-neutral-200 border-slate-200">
             {posts.map((post) => (
               <PostItem key={post.id} post={post} />
             ))}
@@ -55,10 +57,10 @@ export default async function DashboardPage() {
             <EmptyPlaceholder.Description>
               You don&apos;t have any posts yet. Start creating content.
             </EmptyPlaceholder.Description>
-            <PostCreateButton className="border-slate-200 bg-white text-brand-900 hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2" />
+            <PostCreateButton className="bg-white border-slate-200 text-brand-900 hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2" />
           </EmptyPlaceholder>
         )}
       </div>
     </DashboardShell>
-  );
+  )
 }
